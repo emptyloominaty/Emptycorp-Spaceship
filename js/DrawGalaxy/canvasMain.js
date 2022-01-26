78d2c1012f5b0249742f8cba7d424d2c32119208
emptyloominaty <cipuksiblol@gmail.com>
2022-01-27 00:06:05 +0100

    fixed three.js xD

--- js/DrawGalaxy/canvasMain.js
+++ js/DrawGalaxy/canvasMain.js
@@ -1,11 +1,44 @@
+
 class CanvasMain {
-    canvas = document.getElementById("spaceShipWindow")
-    webgl = this.canvas.getContext("webgl")
-
-
     constructor() {
-        this.webgl.clearColor(0.0, 0.0, 0.0, 1.0)
-        this.webgl.clear(this.webgl.COLOR_BUFFER_BIT)
+        //TEST
+        this.scene = new THREE.Scene();
+        this.camera = new THREE.PerspectiveCamera(50, 500 / 400, 0.1, 1000);
+        this.renderer = new THREE.WebGLRenderer( { canvas: spaceShipWindow } )
+
+        this.geometry = new THREE.SphereGeometry(3, 50, 50, 0, Math.PI * 2, 0, Math.PI * 2);
+        this.material1 = new THREE.MeshBasicMaterial();
+        this.material2 = new THREE.MeshBasicMaterial();
+        this.sphere = [new THREE.Mesh(this.geometry, this.material1), new THREE.Mesh(this.geometry, this.material1), new THREE.Mesh(this.geometry, this.material2)];
+
+        this.sphere[0].position.set(1, 1, 1);
+        this.sphere[1].position.set(-1, -1, -1);
+
+        this.scene.add(this.sphere[0]);
+        this.scene.add(this.sphere[1]);
+        this.scene.add(this.sphere[2]);
+
+        this.camera.position.z = 10;
+
+
+        this.hex = "0x" + "000000".replace(/0/g, function() {
+            return (~~(Math.random() * 16)).toString(16);
+        });
+        this.sphere[0].material.color.setHex(this.hex);
+
+        this.hex = "0x" + "000000".replace(/0/g, function() {
+            return (~~(Math.random() * 16)).toString(16);
+        });
+        this.sphere[2].material.color.setHex(this.hex);
+
+
+        this.render = function() {
+            requestAnimationFrame(this.render);
+            this.renderer.render(this.scene, this.camera);
+        };
+
+        this.render();
+
     }
 
 
