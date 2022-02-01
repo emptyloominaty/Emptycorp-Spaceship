b18e99295a931f5a34153d33fd010cba20581b32
emptyloominaty <cipuksiblol@gmail.com>
2022-02-02 00:10:24 +0100

    ..

--- js/objects/ShipParts/weapon.js
+++ js/objects/ShipParts/weapon.js
@@ -2,6 +2,7 @@ class Weapon extends Part {
     doDamage(yaw,pitch,speed) {
         if (this.cooldown>=this.maxCooldown) {
             if (playerShip.usePower((this.power*this.length),this.group)) {
+                let done = false
                 let xyz = this.getStartPosition()
                 let x = playerShip.positionPrecise.x.plus(xyz[0])
                 let y = playerShip.positionPrecise.y.plus(xyz[1])
@@ -14,30 +15,40 @@ class Weapon extends Part {
                     for (let i = 0; i < projectiles.length; i++) {
                         if (projectiles[i] === undefined) {
                             projectiles[i] = new Laser(x, y, z, yaw, pitch, speed, "laser", playerShip, this.damageData.life,this.damageData.color,this.damageData)
+                            done = true
                             break
                         }
                     }
-                    projectiles.push(new Laser(x, y, z, yaw, pitch, speed, "laser", playerShip, this.damageData.life,this.damageData.color,this.damageData))
+                    if (!done) {
+                        projectiles.push(new Laser(x, y, z, yaw, pitch, speed, "laser", playerShip, this.damageData.life, this.damageData.color, this.damageData))
+                    }
                 } else if (this.type==="plasma") {
 
                     for (let i = 0; i < projectiles.length; i++) {
                         if (projectiles[i] === undefined) {
                             projectiles[i] = new Plasma(x, y, z, yaw, pitch, speed, "plasma", playerShip, this.damageData.life,this.damageData.color,this.damageData)
+                            done = true
                             break
                         }
                     }
-                    projectiles.push(new Plasma(x, y, z, yaw, pitch, speed, "plasma", playerShip, this.damageData.life,this.damageData.color,this.damageData))
+                    if (!done) {
+                        projectiles.push(new Plasma(x, y, z, yaw, pitch, speed, "plasma", playerShip, this.damageData.life, this.damageData.color, this.damageData))
+                    }
                 } else if (this.type==="missile") {
                     if (playerShip.missileCargo[this.missileSelect].count>0) {
                         let missileD = playerShip.missileCargo[this.missileSelect].missiledata
                         for (let i = 0; i < projectiles.length; i++) {
                             if (projectiles[i] === undefined) {
                                 projectiles[i] = new Missile(x, y, z, yaw, pitch, speed, "missile", playerShip, missileD.life,missileD.color,this.damageData,missileD)
+                                done = true
                                 break
                             }
                         }
                         playerShip.missileCargo[this.missileSelect].count--
-                        projectiles.push(new Missile(x, y, z, yaw, pitch, speed, "missile", playerShip, missileD.life,missileD.color,this.damageData,missileD))
+                        if (!done) {
+                            projectiles.push(new Missile(x, y, z, yaw, pitch, speed, "missile", playerShip, missileD.life,missileD.color,this.damageData,missileD))
+                        }
+
                     }
                 }
                 this.cooldown = 0
--- js/objects/galaxy/starSystem.js
+++ js/objects/galaxy/starSystem.js
@@ -20,19 +20,25 @@ class StarSystem {
 
     }
 
-    produceResources() {
+
+    produceResources() { //TODO:every min
         for (let i = 0; i<this.producing.length; i++) {
             this.resources[this.producing[i].name] += this.producing[i].amount
         }
     }
 
-    checkResources() {
+    checkResources() { //TODO:every min
         Object.keys(this.resources).forEach(key => {
-            console.log(key, this.resources[key])
             let ratio = this.resources[key].max/this.resources[key].val
+            //idk
             if (ratio>this.resources[key].ratios[2]) {
                 this.resources[key].buying = false
                 this.resources[key].selling = true
+                //remove buy offer
+                if (this.resources[key].need!==undefined) {
+                    this.resourcesNeed[this.resources[name].need] = undefined
+                    this.resources[name].need = undefined
+                }
             } else if (ratio>this.resources[key].ratios[1]) {
                 this.resources[key].buying = true
                 this.resources[key].selling = true
@@ -44,19 +50,59 @@ class StarSystem {
                 this.resources[key].selling = false
             }
 
+            //create buy offer for resource
             if (this.resources[key].need!==undefined && ratio<this.resources[key].ratios[1]) {
                 let amount = (this.resources[key].val*(this.resources[key].ratios[2]-ratio))
-                this.resourcesNeed[this.resourcesNeed.length] = {name:this.resources[key].name, amount:amount, maxPrice:this.resources[key].price}
-                this.resources[key].need = this.resourcesNeed.length
+                let done = false
+                for (let i = 0; i < this.resourcesNeed.length; i++) {
+                    if (this.resourcesNeed[i] === undefined) {
+                        this.resourcesNeed[this.resourcesNeed.length] = {name:this.resources[key].name, amount:amount, maxPrice:this.resources[key].price}
+                        this.resources[key].need = i
+                        done = true
+                        break
+                    }
+                }
+                if (!done) {
+                    this.resourcesNeed[this.resourcesNeed.length] = {name:this.resources[key].name, amount:amount, maxPrice:this.resources[key].price}
+                    this.resources[key].need = this.resourcesNeed.length
+                }
             }
 
+            //increase price
             if (ratio<this.resources[key].ratios[1]) {
                 this.resources[key].maxPrice = this.resources[key].price/((1-this.resources[key].ratios[1])+ratio)
                 this.resourcesNeed[this.resources[key].need] = this.resources[key].maxPrice
             }
-
-
+            this.updateSellingPrice(key,ratio)
         })
+    }
+    
+    updateSellingPrice(name,ratio) {
+        ratio = 0.3+ratio
+        if (ratio>1) {ratio = 1}
+        this.resources[name] = this.resources[name].price/(ratio)
+    }
+
+    buy(name,amount,credits) { //system->ship
+        //TODO:credits
+        this.resources[name].val -= Math.round(amount)
+
+
+        return amount
+    }
+
+    sell(name,amount) { //ship->system
+        let credits
+        this.resources[name].val += Math.round(amount)
+        if (this.resources[name].need!==undefined && this.resourcesNeed[this.resources[name].need]!==undefined) {
+            this.resourcesNeed[this.resources[name].need].amount -= amount
+            if (this.resourcesNeed[this.resources[name].need].amount<0) {
+                this.resourcesNeed[this.resources[name].need] = undefined
+                this.resources[name].need = undefined
+            }
+        }
+        credits = this.resources[name].val*this.resources[name].price
+        return credits
     }
 
 
